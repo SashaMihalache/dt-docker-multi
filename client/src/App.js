633e953143bd5edfcf31c7b79f7fab1bb89040c4
sasha mihalache <sasha.mihalache@gmail.com>
2019-11-15 23:45:17 +0200

    change UI

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App () {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Fib Calculator</h1>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Not found Page</Link>
         </header>
